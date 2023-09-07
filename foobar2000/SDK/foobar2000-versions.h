012b0bc0dfdcf2a379e5d44746207b23814175c0
marc2k3 <marc2003@live.co.uk>
2023-09-07 19:37:07 +0100

    SDK-2023-09-06

--- foobar2000/SDK/foobar2000-versions.h
+++ foobar2000/SDK/foobar2000-versions.h
@@ -2,7 +2,9 @@
 // foobar2000-versions.h
 // foobar2000 SDK version and target API levels are declared in this header
 
-#define FOOBAR2000_TARGET_VERSION 80
+// This SDK does NOT SUPPORT targets older than API 80 / foobar2000 v1.5
+#define FOOBAR2000_TARGET_VERSION 80 // 1.5, 1.6
+// #define FOOBAR2000_TARGET_VERSION 81 // 2.0
 
 #ifdef _M_IX86
 #define FOOBAR2000_TARGET_VERSION_COMPATIBLE 72
